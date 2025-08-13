6cb9c7bc40ee0f28e8abd95d341597d1436860bf
YuuOhnuki <its.yuu.ohnuki@gmail.com>
2025-08-13 22:54:24 +0900

    Vercel関連の依存関係を追加
    📦deps: package.jsonとbun.lockに@vercel/analyticsと@vercel/speed-insightsを追加
    💄style: layout.tsxにSpeedInsightsとAnalyticsコンポーネントを追加し、lang属性をjaに変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -23,7 +25,9 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="ja">
+            <SpeedInsights />
+            <Analytics />
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
         </html>
     );
